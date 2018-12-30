924d5505480303207b2e460b79f239061f625d28
svil1502 <developersvil@gmail.com>
2018-12-30 22:58:26 +0300

    Исправила

--- index.php
+++ index.php
@@ -1,30 +1,4 @@
 <?php
-//если сессия не запущена, запускаем сессию
-if (!session_status())
-{
-    session_start();
-}
-require 'database/QueryBuilder.php';
-require 'components/Auth.php';
-$db=New QueryBuilder();
-$user=New Auth($db);
-//$user->register('user@email.com','bsd');
-
-function redirect($path) {
-    header("Location: $path"); exit;
-}
-/*
-if ($user->login('1','1')){
-    redirect("/");
-}
-else {
-    echo "Такого пользователя нет";
-}
-*/
-var_dump($user->login('user@email.com','bsd'), $_SESSION);
-//$validate->email('user@eaxample.com');
-$user=$user->currentUser();
-echo $user['email'];
 
 //оставляем код отвечающий за работу данной страницы
 $tasks=$db->all("tasks");
--- web/index.php
+++ web/index.php
@@ -1,4 +1,33 @@
 <?php
+
+//если сессия не запущена, запускаем сессию
+if (!session_status())
+{
+    session_start();
+}
+require '../database/QueryBuilder.php';
+require '../components/Auth.php';
+$db=New QueryBuilder();
+$user=New Auth($db);
+//$user->register('user@email.com','bsd');
+
+function redirect($path) {
+    header("Location: $path"); exit;
+}
+/*
+if ($user->login('1','1')){
+    redirect("/");
+}
+else {
+    echo "Такого пользователя нет";
+}
+*/
+var_dump($user->login('user@email.com','bsd'), $_SESSION);
+//$validate->email('user@eaxample.com');
+$user=$user->currentUser();
+echo $user['email'];
+
+//роутинг
 $url=$_SERVER['REQUEST_URI'];
 //var_dump($url);
 if ($url=='/list'){
